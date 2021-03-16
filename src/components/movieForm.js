eeeb9612dcca0409eace99d86ac244eb0a82ee8e
Shohzod-Abdusamatov-7777777 <shohzodabdusamatov7777777@gmail.com>
2021-03-16 23:24:46 +0500

    movies form change

--- src/components/movieForm.js
+++ src/components/movieForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MovieForm = ({match,history}) => {
     return (
-        <div className="mt-2">
+        <div style={{paddingTop:"60px"}}>
             <h2 className="display-5">Movie id - {match.params.id}</h2>
             <button className="btn btn-primary" onClick={()=>{history.replace("/movies")}}>Save</button>
         </div>
