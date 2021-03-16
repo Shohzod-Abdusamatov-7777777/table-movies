0763ab8e342f1acbdf3c06a40a0ae3cac4e41295
Shohzod-Abdusamatov-7777777 <shohzodabdusamatov7777777@gmail.com>
2021-03-16 22:13:29 +0500

    navbar

--- src/components/not-found.js
+++ src/components/not-found.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div style={{paddingTop:"60px"}}>
-            <h1>Not found page</h1>
+            <h1>Not found page 404</h1>
         </div>
     )
 }
